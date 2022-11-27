b0523cbf2caf7282025e5f3abfaadf13c95fe6c6
evamike <mike.eva.89@gmail.com>
2022-11-27 23:00:42 +0100

    first commit

---
+++ src/main/java/com/em/k8spocbackend/K8sPocBackendApplication.java
@@ -0,0 +1,13 @@
+package com.em.k8spocbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sPocBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sPocBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/em/k8spocbackend/K8sPocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.em.k8spocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sPocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
